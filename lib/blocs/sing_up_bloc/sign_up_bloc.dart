4f81304c43e4f16a234b8c5d374df1faa04968e8
RostikKalabishka <rostik31052002@gmail.com>
2024-05-04 16:55:59 +0300

    add registration

--- lib/blocs/sing_up_bloc/sign_up_bloc.dart
+++ lib/blocs/sing_up_bloc/sign_up_bloc.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:webrtc_flutter/domain/repositories/user_repository/models/my_user_model.dart';
 
 import '../../domain/repositories/user_repository/user_repository.dart';
@@ -12,6 +12,20 @@ class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
   SignUpBloc({required UserRepository myUserRepository})
       : userRepository = myUserRepository,
         super(SignUpInitial()) {
-    on<SignUpEvent>((event, emit) async {});
+    on<SignUpEvent>((event, emit) async {
+      if (event is SignUpRequired) {
+        await _registration(event, emit);
+      }
+    });
+  }
+
+  Future<void> _registration(SignUpRequired event, emit) async {
+    emit(SingUpProcess());
+    try {
+      await userRepository.registration(event.myUserModel, event.password);
+      emit(SingUpSuccess());
+    } catch (e) {
+      emit(SingUpFailure(error: e));
+    }
   }
 }
--- lib/blocs/sing_up_bloc/sign_up_state.dart
+++ lib/blocs/sing_up_bloc/sign_up_state.dart
@@ -2,9 +2,22 @@ part of 'sign_up_bloc.dart';
 
 sealed class SignUpState extends Equatable {
   const SignUpState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class SignUpInitial extends SignUpState {}
+
+class SingUpProcess extends SignUpState {}
+
+class SingUpSuccess extends SignUpState {}
+
+class SingUpFailure extends SignUpState {
+  final Object error;
+
+  const SingUpFailure({required this.error});
+
+  @override
+  List<Object> get props => super.props..add(error);
+}
